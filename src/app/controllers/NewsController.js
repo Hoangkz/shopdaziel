4d574209135e5672921943e0b1fc542d8d42af94
hoang <hoangyl245@gmail.com>
2022-08-24 21:14:10 +0700

    f

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,7 +1,7 @@
-// const Course = require('../modals/Item');
-// const jwt = require('jsonwebtoken');
-// const user = require('../modals/user')
-// const {mutipleMongooseToObject} = require('../../util/mongoose')
+const Course = require('../modals/Item');
+const jwt = require('jsonwebtoken');
+const user = require('../modals/user')
+const {mutipleMongooseToObject} = require('../../util/mongoose')
 
 class NewsController{
 
