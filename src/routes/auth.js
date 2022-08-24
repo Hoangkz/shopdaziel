f8dc4ff30eef808078813317baddda70b94356cc
hoang <hoangyl245@gmail.com>
2022-08-24 23:42:54 +0700

    data

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,16 @@
-// const express = require('express');
-// const getUser = require("../app/controllers/checkuser")
+const express = require('express');
+const getUser = require("../app/controllers/checkuser")
 
-// const router = express.Router();
+const router = express.Router();
 
-// const AuthController = require('../app/controllers/AuthController');
+const AuthController = require('../app/controllers/AuthController');
 
-// router.get('/login', AuthController.login);
-// router.post('/login', AuthController.setLogin);
-// router.post('/logout', AuthController.logout);
-// router.get('/signup', AuthController.signup);
-// router.get('/user', getUser.getuser,AuthController.getuser);
+router.get('/login', AuthController.login);
+router.post('/login', AuthController.setLogin);
+router.post('/logout', AuthController.logout);
+router.get('/signup', AuthController.signup);
+router.get('/user', getUser.getuser,AuthController.getuser);
 
 
-// module.exports = router;
+module.exports = router;
 
