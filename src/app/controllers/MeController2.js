81dc54ae46b236b9c4b25cff66eb4c1fe707e7a8
hoang <hoangyl245@gmail.com>
2022-08-24 20:33:59 +0700

    x

--- src/app/controllers/MeController2.js
+++ src/app/controllers/MeController2.js
@@ -1,10 +1,10 @@
 
 
-class MeController{
+class MeController2{
 
     show(req,res,next){
         res.render('home')
     }
 }
 
-module.exports = new MeController();
+module.exports = new MeController2();
