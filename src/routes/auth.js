e27834bec1e85ffa7d4e0dadc6692232f1a60c63
hoang <hoangyl245@gmail.com>
2022-08-25 09:17:15 +0700

    e

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const getUser = require("../app/controllers/checkuser")
+const getUser = require("../app/controllers/checkuser")
 
 const router = express.Router();
 
