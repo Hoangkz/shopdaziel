bb1708216d9e0af8bc21f0c8d0fccbcc7346a42a
hoang <hoangyl245@gmail.com>
2022-08-25 17:54:41 +0700

    r

--- src/routes/me.js
+++ src/routes/me.js
@@ -4,12 +4,6 @@
 // const express = require('express');
 // const router = express.Router();
 
-// const meController = require('../app/controllers/MeController');
-
-// //newcontroller.index
-// router.get('/trash/items', meController.trashCourse);
-
-// router.get('/stored/items', meController.storedCourses);
 
 // module.exports = router;
 
@@ -20,9 +14,15 @@ const express = require('express');
 const router = express.Router();
 
 const meController = require('../app/controllers/MeController2');
+const meController = require('../app/controllers/MeController');
 
 //newcontroller.index
 router.get('/', meController.show);
 
+
+//newcontroller.index
+router.get('/trash/items', meController.trashCourse);
+
+router.get('/stored/items', meController.storedCourses);
 module.exports = router;
 
