46253f2d786258a3aeba1d296f85c168bc1abd30
hoang <hoangyl245@gmail.com>
2022-08-26 02:40:51 +0700

    t

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-let link ="mongodb+srv://hoang:111@cluster0.glxu3.mongodb.net/Dalziel?retryWrites=true&w=majority"
+let link ="mongodb+srv://hoang:auJ4wXxOkTn3lR9C@cluster0.glxu3.mongodb.net/Dalziel?retryWrites=true&w=majority"
 async function connect(){
     try {
         await mongoose.connect(link,{
