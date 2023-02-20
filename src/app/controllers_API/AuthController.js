83a7545dd833445be4ec441ca839f243da8d133e
hoang <hoangyl245@gmail.com>
2023-02-21 04:15:29 +0700

    update

--- src/app/controllers_API/AuthController.js
+++ src/app/controllers_API/AuthController.js
@@ -7,7 +7,7 @@ class AuthController {
     saveProfile(req, res, next) {
         let data = req.body
         data.checkuser = 1
-        return res.json({ message: "error" })
+        return res.status(400).json({ message: "error" })
         user.updateOne({ _id: req.params.id }, data)
             // redirect chuyển sang trang ....
             .then(() => {
@@ -31,7 +31,7 @@ class AuthController {
             .then(data => {
                 if (data == null) {
                     newUser.save()
-                        .then(() => res.json({ message: 'Đăng ký tài khoản thành công' }))
+                        .then(() => res.status(200).json({ message: 'Đăng ký tài khoản thành công' }))
                         .catch(next)
                 }
                 else {
@@ -63,40 +63,31 @@ class AuthController {
             })
             .then(data => {
                 if(!data){
-                    res.status(400).json({ message: "Thông tin tài khoản hoặc mật khẩu không chính xác" })
+                    return res.status(400).json({ message: "Thông tin tài khoản hoặc mật khẩu không chính xác" })
                 }
                 else{
                     data = JSON.parse(data)
                     const { password, ...rest } = data
-                    const token = jwt.sign({ data: rest }, "mk", {
-                        expiresIn: "60s"
-                    })
-                    const refresh_token = jwt.sign({ data: rest }, "refresh", {
-                        expiresIn: "360d"
-                    })
+                    const token = jwt.sign({ data: rest }, "mk", { expiresIn: '5s' })
+                    const refresh_token = jwt.sign({ id: rest.id,username: rest.username,gender:rest.gender}, "refresh", { expiresIn: "15s"})
                     const newToken = new List_Token({refresh_token});
                     newToken.save()
-                    .then(() => {
-                        return res.json({
-                            message: "Đăng nhập thành công",
-                            token: token,
-                            refresh_token: refresh_token
-                        })
-                    })
-                    .catch(err => {
-                        res.status(500).json({message:"lỗi sever"})
+                    console.log("vao day")
+                    return res.status(200).json({
+                        message: "Đăng nhập thành công",
+                        token: token,
+                        refresh_token: refresh_token
                     })
                 }
 
             })
             .catch(err => {
-                res.status(500).json({message:"lỗi sever"})
+                return res.status(500).json({message:"lỗi sever"})
             })
     }
     //Middleware
     accessToken(req, res, next) {
-        const authorization = req.header["authorization"]|
-        console.log(authorization)
+        const authorization = req.header.authorization
         const token = authorization.splid(" ")[1]
         if(!token)res.status(403).json({message: "Bạn không có quyền truy cập !"})
         jwt.verify(data,"mk",(err, data)=>{
@@ -110,34 +101,47 @@ class AuthController {
     refreshToken(req, res, next) {
         const refreshToken = req.body.refresh_token;
         if(!refreshToken){
-            return res.status(401).json("token không hợp lệ")
+            return res.status(401).json({
+                message: "Thời gian truy cập của bạn đã hết!",
+            }) 
         }
-        List_Token.findOne({refresh_token:refreshToken})
-        .then((data)=>{
-            if(!data){
-                return res.status(401).json("token không hợp lệ")
-            }
-            jwt.verify(refreshToken,"refresh",(err, decoded)=>{
-                if (err) {
-                    res.status(401).json("Lỗi token không hợp lệ")                  
+        else{
+            List_Token.findOne({refresh_token:refreshToken})
+            .then((data)=>{
+                if(!data){
+                    return res.status(401).json({
+                        message: "Thời gian truy cập của bạn đã hết!",
+                    }) 
                 }
-                const token =  jwt.sign({decoded},"mk", {expiresIn: "60s"})
-                res.json({
-                    message: "Refresh token thành công",
-                    token: token,
-                })
+                else{
+                    jwt.verify(refreshToken,"refresh",(err, decoded)=>{
+                        if (err) {
+                            return res.status(401).json({
+                                message: "Thời gian truy cập của bạn đã hết!",
+                            })                  
+                        }
+                        else{
+                            const token =  jwt.sign({data:decoded.data},"mk", { expiresIn: "5s" })
+                            return res.status(200).json({
+                                message: "Refresh token thành công",
+                                token: token,
+                            })
+                        }
+                    })
+                }
             })
-        })
-        .catch((error)=>{
-            res.status(500).json({message:"lỗi sever"})
-        })
+            .catch((error)=>{
+                res.status(500).json({message:"lỗi sever"})
+            })
+        }
         
     }
     logout(req, res,next) {
-        const refreshToken = req.body.token
+        console.log(req.body)
+        const refreshToken =req.body.refresh_token|| req.headers?.authorization?.split(' ')[1]|null
         List_Token.deleteOne({refresh_token: refreshToken})
-        .then(()=> res.status(200).json({messsage:"Bạn đã đăng xuất!"}))
-        .catch(()=>res.json("lỗi sever"))
+        .then(()=> res.status(200).json({message:"Tài khoản của bạn đã được đăng xuất!"}))
+        .catch(()=>res.status(500).json({message:"lỗi sever"}))
     }
 
 }
--- src/routes/API/authRouter.js
+++ src/routes/API/authRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const AuthController = require('../../app/controllers_API/AuthController');
 // đăng ký đăng nhập
 router.post('/refresh-token', AuthController.refreshToken);
+router.post('/logout', AuthController.logout);
 router.post('/token', AuthController.accessToken);
 router.post('/login', AuthController.setLogin);
 router.post('/signup', AuthController.saveAccount);
