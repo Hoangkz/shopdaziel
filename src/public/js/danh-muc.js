7ab6f554a1d27b80a434ff0ebfac0dce1f6baf49
hoangkz <hoangyl245@gmail.com>
2023-05-22 08:14:36 +0700

    update

---
+++ src/public/js/danh-muc.js
@@ -0,0 +1,92 @@
+const a687f3967b7c2fe6a134a2c11894eea4b_tn = "/img/file/687f3967b7c2fe6a134a2c11894eea4b_tn.png"
+const a31234a27876fb89cd522d7e3db1ba5ca_tn = "/img/file/31234a27876fb89cd522d7e3db1ba5ca_tn.png"
+const a978b9e4cb61c611aaaf58664fae133c5_tn = "/img/file/978b9e4cb61c611aaaf58664fae133c5_tn.png"
+const ac3f3edfaa9f6dafc4825b77d8449999d_tn = "/img/file/c3f3edfaa9f6dafc4825b77d8449999d_tn.png"
+const a74ca517e1fa74dc4d974e5d03c3139de_tn = "/img/file/74ca517e1fa74dc4d974e5d03c3139de_tn.png"
+const a7abfbfee3c4844652b4a8245e473d857_tn = "/img/file/7abfbfee3c4844652b4a8245e473d857_tn.png"
+const a18fd9d878ad946db2f1bf4e33760c86f_tn = "/img/file/18fd9d878ad946db2f1bf4e33760c86f_tn.png"
+const ac432168ee788f903f1ea024487f2c889_tn = "/img/file/c432168ee788f903f1ea024487f2c889_tn.png"
+const acd8e0d2e6c14c4904058ae20821d0763_tn = "/img/file/cd8e0d2e6c14c4904058ae20821d0763_tn.png"
+const a75ea42f9eca124e9cb3cde744c060e4d_tn = "/img/file/75ea42f9eca124e9cb3cde744c060e4d_tn.png"
+const a36013311815c55d303b0e6c62d6a8139_tn = "/img/file/36013311815c55d303b0e6c62d6a8139_tn.png"
+const a8e71245b9659ea72c1b4e737be5cf42e_tn = "/img/file/8e71245b9659ea72c1b4e737be5cf42e_tn.png"
+const a49119e891a44fa135f5f6f5fd4cfc747_tn = "/img/file/49119e891a44fa135f5f6f5fd4cfc747_tn.png"
+const a3fb459e3449905545701b418e8220334_tn = "/img/file/3fb459e3449905545701b418e8220334_tn.png"
+const a86c294aae72ca1db5f541790f7796260_tn = "/img/file/86c294aae72ca1db5f541790f7796260_tn.png"
+const a099edde1ab31df35bc255912bab54a5e_tn = "/img/file/099edde1ab31df35bc255912bab54a5e_tn.png"
+const aef1f336ecc6f97b790d5aae9916dcb72_tn = "/img/file/ef1f336ecc6f97b790d5aae9916dcb72_tn.png"
+const ace8f8abc726cafff671d0e5311caa684_tn = "/img/file/ce8f8abc726cafff671d0e5311caa684_tn.png"
+const danhmuc =[
+    {
+        img:a687f3967b7c2fe6a134a2c11894eea4b_tn,
+        desc:"Thời trang nam"
+    },
+    {
+        img:a31234a27876fb89cd522d7e3db1ba5ca_tn,
+        desc:"Điện thoại và phụ kiện"
+    },
+    {
+        img:a978b9e4cb61c611aaaf58664fae133c5_tn,
+        desc:"Thiết bị điện tử"
+    },
+    {
+        img:ac3f3edfaa9f6dafc4825b77d8449999d_tn,
+        desc:"Máy tính và lattop"
+    },
+    {
+        img:a74ca517e1fa74dc4d974e5d03c3139de_tn,
+        desc:"Giày dép"
+    },
+    {
+        img:a7abfbfee3c4844652b4a8245e473d857_tn,
+        desc:"Thiết bị điện tử gia dụng"
+    },
+    {
+        img:a18fd9d878ad946db2f1bf4e33760c86f_tn,
+        desc:"Balo và túi ví nam, nữ"
+    },
+    {
+        img:ace8f8abc726cafff671d0e5311caa684_tn,
+        desc:"Đồ chơi"
+    },
+    {
+        img:acd8e0d2e6c14c4904058ae20821d0763_tn,
+        desc:"Giặt giũ và chăm sóc nhà cửa"
+    },
+    {
+        img:a75ea42f9eca124e9cb3cde744c060e4d_tn,
+        desc:"Thời trang nữ"
+    },
+    {
+        img:a36013311815c55d303b0e6c62d6a8139_tn,
+        desc:"Nhà sách online"
+    },
+    {
+        img:a8e71245b9659ea72c1b4e737be5cf42e_tn,
+        desc:"Phụ kiện và trang sức"
+    },
+    {
+        img:a49119e891a44fa135f5f6f5fd4cfc747_tn,
+        desc:"Sức khoẻ"
+    },
+    {
+        img:a3fb459e3449905545701b418e8220334_tn,
+        desc:"Ôtô, xe máy xe đạp"
+    },
+    {
+        img:a86c294aae72ca1db5f541790f7796260_tn,
+        desc:"Đồng hồ"
+    },
+    {
+        img:a099edde1ab31df35bc255912bab54a5e_tn,
+        desc:"Đồ dùng cho trẻ em"
+    },
+    {
+        img:aef1f336ecc6f97b790d5aae9916dcb72_tn,
+        desc:"Chăm sóc sắc đẹp"
+    },
+    {
+        img:ac432168ee788f903f1ea024487f2c889_tn,
+        desc:"Bách hoá online"
+    }
+]
